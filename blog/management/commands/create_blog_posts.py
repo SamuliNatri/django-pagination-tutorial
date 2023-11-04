869874b8e3c20dc93a65ec56d05efbf0a583c076
Samuli Natri <samuli.natri@gmail.com>
2023-11-05 00:58:35 +0200

    moar

--- blog/management/commands/create_blog_posts.py
+++ blog/management/commands/create_blog_posts.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         title = "How To Make Money"
         body = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum saepe aliquid voluptatibus voluptas nam dolorum modi minima unde?"
-        for i in range(10):
+        for i in range(15):
             slug="how-to-make-money-%s" % i
             Post.objects.create(title=title, body=body, slug=slug)
             self.stdout.write(self.style.SUCCESS('Successfully created a blog post'))
