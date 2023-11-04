b48d48d27e75c04ef334722376b5397a07ed196f
Samuli Natri <samuli.natri@gmail.com>
2023-11-05 00:39:48 +0200

    add pagination

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from blog.views import PostDetailView
+from blog.views import PostDetailView, BlogView
+from blog.views import index2
 
 app_name = 'blog'
 
 urlpatterns = [
+    path('', BlogView.as_view(), name='blog'),
+    path('index2/', index2, name='index2'),
     path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,26 @@
+from django.shortcuts import render
 from django.views.generic import DetailView
+from django.views.generic import ListView
+from django.core.paginator import Paginator
 from blog.models import Post
 
+class BlogView(ListView):
+    model = Post
+    paginate_by = 5
+    # template_name = 'blog/index.html'
+    template_name = 'blog/blue_theme.html'
+    context_object_name = 'posts'
 
 class PostDetailView(DetailView):
     model = Post
+
+
+def index2(request):
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 5)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+
+    return render(request,
+                  'blog/index2.html',
+                  {'posts': posts})
